63bb005061f81c95b8b1059c8721afa9903533f7
Eric Chee <eric.chee@hulu.com>
2021-04-06 12:06:17 -0700

    Added example code for textviews, constraintlayouts, drawable/color/string/dimens resources, view visibility, elipsizing and more

--- app/src/main/java/edu/uw/echee/moreviewslayouts/MainActivity.kt
+++ app/src/main/java/edu/uw/echee/moreviewslayouts/MainActivity.kt
@@ -2,13 +2,32 @@ package edu.uw.echee.moreviewslayouts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+
+import edu.uw.echee.moreviewslayouts.databinding.UpdateViewsProgrammaticallyBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: UpdateViewsProgrammaticallyBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = UpdateViewsProgrammaticallyBinding.inflate(layoutInflater).apply { setContentView(root) }
+
+        val shouldShowButton = false
+
+        binding.btnLoad.setOnClickListener {
+            binding.tvWelcome.setTextColor(getColor(R.color.default_text_color))
+            binding.tvWelcome.text = getString(R.string.welcome_eric)
+
+            binding.btnLoad.visibility = if (shouldShowButton){
+                View.VISIBLE
+            } else {
+                View.GONE
+            }
+        }
     }
 }
+
 
 /**
  * ......
